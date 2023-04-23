a0756d22d3d41771f2aabdfcba63fe7c6c239b8d
stas <bnrg@yandex.ru>
2023-04-23 11:48:27 +0530

    two

--- html/js/registration.js
+++ html/js/registration.js
@@ -3,9 +3,17 @@ function onsubmit(event) {
     var email = document.getElementById('email').value;
 
     // TODO: Проверить наличие email, и если email нет то вывести ошибку "Введите email"
+ 
+    if (email.trim().length === 0) {
+
+        alert('Введите email');
+        event.preventDefault();
+        
+        return;
+    }
 
     // Проверяем email
-    if (email.includes('@') === false) {
+    if (email.includes('') === false) {
         // Не email
         alert('Введите email');
         event.preventDefault();
@@ -14,7 +22,21 @@ function onsubmit(event) {
     }
 
     // TODO: Проверить наличие пароля, и если пароля нет то вывести ошибку "Введите пароль"
+    var password = document.getElementById('password').value
+    if(password.length === 0) {
+         alert('Введите пароль');
+         event.preventDefault();
+          
+           return;
+    }
     // TODO: Проверить совпадение паролей, и если пароли не совпадают то вывести ошибку "Пароли не совпадают"
+    var password = document.getElementById('password').value
+    if(password !== confirm_password) {
+         alert('Пароли не совпадают');
+         event.preventDefault();
+          
+           return;
+       }
 }
 
 function onload() {
