edf2912978dc96d4a2f23c4d9e027e15c2c4cbed
tupkalov <tupkalov@gmail.com>
2023-04-24 19:39:34 +0300

    update server.js

--- server/server.js
+++ server/server.js
@@ -2,46 +2,56 @@ const http = require('http');
 const fs = require('fs');
 const path = require('path');
 
-const server = http.createServer((req, res) => {
+// Метод вызывается на каждый запрос к серверу
+function onRequest(req, res) {
   console.log(`Incoming request for ${req.url}`);
   
-  if (req.url === '/') {
-    // if user visits root path, redirect to index.html
-    res.writeHead(302, { 'Location': '/index' });
-    res.end();
-
-
-  } else if (req.url.startsWith('/static/')) {
-    // if user requests a file in /static directory, return the file
-    const filename = path.join(__dirname, '..', req.url);
-    const stream = fs.createReadStream(filename);
-
-    stream.on('error', err => {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.write('404 Not Found\n');
-      res.end();
-    });
-
-    stream.pipe(res);
-
-
+  if (req.url.startsWith('/static/')) {
+    returnStatic(req, res);
   } else {
-    // if user requests an html page, return the html file
-    const filename = path.join(__dirname, '../html', `${req.url}.html`);
-    const stream = fs.createReadStream(filename);
-
-    stream.on('error', err => {
-      res.writeHead(404, { 'Content-Type': 'text/plain' });
-      res.write('404 Not Found\n');
-      res.end();
-    });
-
-    res.writeHead(200, { 'Content-Type': 'text/html' });
-    stream.pipe(res);
+    returnHtml(req, res);
   }
-});
+
+}
+
+// Возвращает html-страницу и обрабатывать запросы
+function returnHtml (req, res) {
+  // if user requests an html page, return the html file
+  const filename = path.join(__dirname, '../html', `${req.url}.html`);
+  const stream = fs.createReadStream(filename);
+
+  stream.on('error', err => {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.write('404 Not Found\n');
+    res.end();
+  });
+
+  res.writeHead(200, { 'Content-Type': 'text/html' });
+  stream.pipe(res);
+}
+
+// Возвращает статический файл
+function returnStatic(req, res) {
+  // if user requests a file in /static directory, return the file
+  const filename = path.join(__dirname, '..', req.url);
+  const stream = fs.createReadStream(filename);
+
+  stream.on('error', err => {
+    res.writeHead(404, { 'Content-Type': 'text/plain' });
+    res.write('404 Not Found\n');
+    res.end();
+  });
+
+  stream.pipe(res);
+}
+
+const server = http.createServer(onRequest);
 
 const port = 3000;
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+
+
+
